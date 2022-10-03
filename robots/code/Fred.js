5c39b0b0109425f3d87d9ed7e3affce8104e5bfe
jhodk <john_hodkinson@hotmail.co.uk>
2022-10-03 23:51:04 +0100

    Add default robots

---
+++ robots/code/Fred.js
@@ -0,0 +1,9 @@
+function foo(gameState) {
+    return {
+        rotateTurret: "anticlockwise",
+        moveTank: "none",
+        rotateTank: "none",
+        fire: true
+    };
+}
+module.exports = foo;
